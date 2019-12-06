ed5e3d1ba1a21865a1b179a1dd03fb0d7f58a2f6
mntky <djminatoo@gmail.com>
2019-12-06 17:05:08 +0900

    add update

---
+++ cmd/update.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	//"encoding/json"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"woctl/pkg"
+	//"github.com/lxc/lxd/shared/api"
+)
+
+func init() {
+	RootCmd.AddCommand(newupdateCmd())
+}
+
+func newupdateCmd() *cobra.Command {
+	updateCmd := &cobra.Command {
+		Use:		"update",
+		Short:	"update lxc",
+		Run:		func(cmd *cobra.Command, args []string) {
+			endpointurl := viper.GetString("url")
+
+			containername, err := cmd.Flags().GetString("name")
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			yamlfilename, err := cmd.Flags().GetString("file")
+			if err != nil {
+				fmt.Println(err)
+			}
+			//TODO yamlの構文チェック機能をつけたい。
+			spec, err := ioutil.ReadFile(yamlfilename)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			//body
+			pdata := pkg.Postdata{
+				endpointurl,
+				containername,
+				"update",
+				spec,
+			}
+			err = pkg.Send(pdata)
+			if err != nil {
+				fmt.Println(err)
+			}
+		},
+	}
+
+	updateCmd.PersistentFlags().StringP("name", "n", "", "container name")
+	updateCmd.PersistentFlags().StringP("file", "f", "", "select yaml path")
+	return updateCmd
+}
+
